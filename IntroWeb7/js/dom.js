79068469bd14bd15a9a9983dcf43064c638d9f58
carlos <carlos_741@hotmail.es>
2023-02-09 18:08:15 -0600

    Se realizo intro web 6 y 7

---
+++ IntroWeb7/js/dom.js
@@ -0,0 +1,4 @@
+function evento (){
+    document.write("Hola esra un una introduccion al js con funciones y dom")
+    alert("Ya fuimos al pipirin ")
+}
---
+++ IntroWeb7/js01/tiposdedatosyvariables.js
@@ -0,0 +1,11 @@
+var nombre = "josue"
+var edad = 30
+var tengoMascotas = true
+var tengoTrabajo = false
+var tengoSueldo //undefinied
+
+let nombre = "josue"
+let edad = 30
+let tengoMascotas = true
+let tengoTrabajo = false
+let tengoSueldo //undefinied
