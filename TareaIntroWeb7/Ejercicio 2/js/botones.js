ce0540ed8ba7e6905768d409d970ee835f1e3a0d
carlos <carlos_741@hotmail.es>
2023-02-13 02:03:14 -0600

    Se agrego la tarea de introweb7 JS y Dom

---
+++ TareaIntroWeb7/Ejercicio 2/js/botones.js
@@ -0,0 +1,12 @@
+
+
+let Boton1 = document.querySelector("#Boton1");
+Boton1.addEventListener("click", function() {
+  alert("En Japón existe también un lugar donde puedes bañarte y nadar en piscinas de café, Yunessun Spa Resort.");
+});
+
+
+let Boton2 = document.querySelector("#Boton2");
+Boton2.addEventListener("click", function() {
+  alert("Los efectos del café disminuyen cuando se mezcla con leche.");
+});
